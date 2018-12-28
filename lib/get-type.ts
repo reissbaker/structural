e0ee136b23c4a715c3b826fd90806644deb0c4d5
Jake Teton-Landis <just.1.jake@gmail.com>
2018-12-27 17:22:30 -0800

    GetType only takes Types

--- lib/get-type.ts
+++ lib/get-type.ts
@@ -1,6 +1,7 @@
 import { Type } from "./type";
 
 type GenericFn<T, R extends Type<T>> = (...a: any[]) => R;
-export type GetType<T> = T extends Type<infer U> ? U :
+
+export type GetType<T extends Type<any>> = T extends Type<infer U> ? U :
                          (T extends GenericFn<any, infer R> ?
                            (R extends Type<infer U> ? U : never) : never);
